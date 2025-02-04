366b4d07b9a9182f34d9feb96d3aadb012e2e4cd
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 23:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.length < 1) return false;
+	let email = /^[a-zA-Z0-9]+(?:[.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:[.-]?[a-zA-Z0-9]+)*\.[a-zA-Z]{2,3}$/;
+	return email.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
+
